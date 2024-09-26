48104899ceba7e84978196bcb7809308dd457b8e
Nandini Singh <nishasingh5m@gmail.com>
2024-09-26 06:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
-import PyPDF
-from PyPDF import PdfReader
+import PyPDF2
+from PyPDF2 import PdfReader
 from streamlit_extras.add_vertical_space import add_vertical_space
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.embeddings import SentenceTransformerEmbeddings
